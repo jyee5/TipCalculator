67becde30fc467233294f5364cf2f70b1489f09b
Justin Yee <justin.yee529@gmail.com>
2020-05-25 14:40:50 -0700

    Adding updated CSS/HTML and adding Javascript

---
+++ resources/tipcalculator.js
@@ -0,0 +1,38 @@
+function calculateTip() {
+    var cost = document.getElementById("cost").value;
+    var service = document.getElementById("service").value;
+    var numOfPeople = document.getElementById("numPeople").value;
+  
+    if (cost === "" || service == 0) {
+      alert("Please enter values");
+      return;
+    }
+    //Check to see if this input is empty or less than or equal to 1
+    if (numOfPeople === "" || numOfPeople <= 1) {
+      numOfPeople = 1;
+      document.getElementById("each").style.display = "none";
+    } else {
+      document.getElementById("each").style.display = "block";
+    }
+
+    //Calculate tip
+    let total = (cost * service) / numOfPeople;
+    //round to two decimal places
+    total = Math.round(total * 100) / 100;
+    //next line allows us to always have two digits after decimal point
+    total = total.toFixed(2);
+    //Display the tip
+    document.getElementById("total-tip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
+  
+}
+  
+  //Hide the tip amount on load
+  document.getElementById("total-tip").style.display = "none";
+  document.getElementById("each").style.display = "none";
+  
+  //click to call function
+  document.getElementById("calculate").onclick = function() {
+      calculateTip();
+    
+  };
